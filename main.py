16fb419f39298154956cee2bd91b40813c64ff5b
Gabymeow <avatarko.994@gmail.com>
2021-05-15 15:54:00 +0300

    Add win support

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 home = os.path.expanduser("~")
+#print(home)
 sdfolder = os.path.join(home, "fakeSD")
-print(sdfolder)
+#print(sdfolder)
 
 for file in os.listdir(sdfolder):
     if file.endswith(".NEF"):
